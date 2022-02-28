51cd4475705f5f542edb8063274cc3506a5e671f
TA-Sakin <tasakin98@gmail.com>
2022-03-01 02:35:47 +0600

    details data fetched

--- js/script.js
+++ js/script.js
@@ -25,22 +25,31 @@ const showPhone = (phones) => {
   }
   console.log(phones);
   for (const phone of phones) {
+    console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col-lg-4");
     div.innerHTML = `
           <div class="card mb-5 mt-5" style="width: 18rem">
-                  <img src="${phone.image}" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h4 class="card-title">${phone.phone_name}</h4>
-                    <h5 class="card-title">${phone.brand}</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Details</a>
-                  </div>
-                </div>
+            <img src="${phone.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h4 class="card-title">${phone.phone_name}</h4>
+              <h5 class="card-title">${phone.brand}</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </p>
+              <a href="#" onclick="details('${phone.slug}')" class="btn btn-primary">Details</a>
+            </div>
+          </div>
           `;
     phoneContainer.appendChild(div);
   }
+};
+
+// -----------------Details button----------------
+const details = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
 };
