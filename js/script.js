9c76d7fd15b43842301279c5ef5e1bdac453fd89
TA-Sakin <tasakin98@gmail.com>
2022-03-01 03:20:05 +0600

    details added

--- js/script.js
+++ js/script.js
@@ -28,8 +28,9 @@ const showPhone = (phones) => {
     console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col-lg-4");
+    div.classList.add("col-md-6");
     div.innerHTML = `
-          <div class="card mb-5 mt-5" style="width: 18rem">
+          <div class="card mb-5 mt-5 text-center" style="width: 18rem">
             <img src="${phone.image}" class="card-img-top" alt="..." />
             <div class="card-body">
               <h4 class="card-title">${phone.phone_name}</h4>
@@ -38,7 +39,7 @@ const showPhone = (phones) => {
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
               </p>
-              <a href="#" onclick="details('${phone.slug}')" class="btn btn-primary">Details</a>
+              <a href="#" onclick="detailsBtn('${phone.slug}')" class="btn btn-primary">Details</a>
             </div>
           </div>
           `;
@@ -47,9 +48,31 @@ const showPhone = (phones) => {
 };
 
 // -----------------Details button----------------
-const details = (id) => {
+const detailsBtn = (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => phoneDetails(data.data));
+};
+
+// -----------------Show details on UI----------------
+const phoneDetails = (phone) => {
+  console.log(phone);
+  const phnDetails = document.getElementById("phone-details");
+  phnDetails.innerHTML = `
+          <div class="card mb-5 mt-5" style="width: 25rem">
+            <img src="${phone.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h4 class="card-title">${phone.name}</h4>
+              <h5 class="card-title">${phone.brand}</h5>
+              <h6 class="card-title">${(phone.releaseDate = phone.releaseDate
+                ? phone.releaseDate
+                : "No release date found")}</h6>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          `;
 };
