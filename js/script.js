a032bc2e9401cea69ae8891ffdfcd0eb3d85a026
TA-Sakin <tasakin98@gmail.com>
2022-03-01 18:21:32 +0600

    bonus added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // -----------------Search button----------------
+//-----------------------------------------------
 const searchBtn = () => {
   const searchText = document.getElementById("search-box").value;
   if (searchText !== "") {
@@ -6,17 +7,23 @@ const searchBtn = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        showPhone(data.data);
-        document.getElementById("error-msg").style.display = "none";
+        if (data.data.length === 0) {
+          document.getElementById("error-msg").style.display = "block";
+        } else {
+          showPhone(data.data);
+          document.getElementById("error-msg").style.display = "none";
+        }
       });
     document.getElementById("search-box").value = "";
     document.getElementById("phone-container").innerHTML = "";
+    document.getElementById("phone-details").innerHTML = "";
   } else {
     document.getElementById("error-msg").style.display = "block";
   }
 };
 
 // -----------------Show phone on UI----------------
+//--------------------------------------------------
 const showPhone = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
   if (phones.length >= 20) {
@@ -43,7 +50,8 @@ const showPhone = (phones) => {
   }
 };
 
-// -----------------Details button----------------
+// -----------------Details button------------------
+//--------------------------------------------------
 const detailsBtn = (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
@@ -52,38 +60,55 @@ const detailsBtn = (id) => {
 };
 
 // -----------------Show details on UI----------------
+//----------------------------------------------------
 const phoneDetails = (phone) => {
   console.log(phone);
   const phnDetails = document.getElementById("phone-details");
-
   phnDetails.innerHTML = `
   <div class="card mb-5 mt-5 h-100" style="width: 30rem;">
   <img src="${phone.image}" class="card-img-top" alt="..." />
-  <div class="card-body">
-              <h4 class="card-title text-center">${phone.name}</h4>
-              <h5 class="card-title text-center">${phone.brand}</h5>
-              <ul id="ul" class="list-group list-group-flush ">
-                    <li class="list-group-item fw-bold text-center">${(phone.releaseDate =
-                      phone.releaseDate
-                        ? phone.releaseDate
-                        : "No release date found")}</li>
-                    <li class="list-group-item"><span class="bold">Sensors: </span> ${phone.mainFeatures.sensors.join(
-                      ", "
-                    )}.</li>
-                    
-                  </ul>
-            </div>
-          </div>
-          `;
+    <div class="card-body">
+        <h3 class="card-title text-center">${phone.name}</h3>
+        <h4 class="card-title text-center">${phone.brand}</h4>
+        <ul id="ul" class="list-group list-group-flush ">
+          <li class="list-group-item text-center">${(phone.releaseDate =
+            phone.releaseDate
+              ? phone.releaseDate
+              : "No release date found")}</li>
+          <li class="list-group-item fw-bold text-center">
+          Main Features
+          </li>
+        </ul>
+    </div>
+  </div>
+`;
+  //----------------- Main features and others data ---------------------
+  //-----------------------------------------------------------------------
+  moreInfo(phone.mainFeatures);
+  moreInfo(phone.others, phone, true);
+};
+
+//----------------- Main features and others data ---------------------
+//-----------------------------------------------------------------------
+const moreInfo = (info, phone, enter) => {
   const ul = document.getElementById("ul");
-  for (const key in phone.others) {
+  if (enter && phone.others) {
     const li = document.createElement("li");
     li.classList.add("list-group-item");
-    li.innerHTML = ` 
-                <span class="bold">  ${phone.others ? key : ""}: </span>${
-      phone.others[key]
-    }
-            `;
+    li.innerHTML = `<li id='remove-dot' class="fw-bold text-center">Others
+    </li>`;
+    ul.appendChild(li);
+  }
+  for (const key in info) {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = `
+                  <span class="bold">  ${
+                    info ? key[0].toUpperCase() + key.slice(1) : ""
+                  }: </span>${(info[key] =
+      key === "sensors" ? info[key].join(", ") : info[key])}
+              `;
+    console.log(info[key]);
     ul.appendChild(li);
   }
 };
