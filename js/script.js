f32ee45e1ba508cab90880c376fd74c1f404bce5
TA-Sakin <47474230+TA-Sakin@users.noreply.github.com>
2022-03-07 20:50:45 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,57 +2,48 @@
 //-----------------------------------------------
 const searchBtn = () => {
   const searchText = document.getElementById("search-box").value;
+  document.getElementById("search-box").value = "";
   document.getElementById("spinner").style.display = "block";
-  if (searchText !== "") {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.data.length === 0) {
-          document.getElementById("error-msg").style.display = "block";
-          document.getElementById("spinner").style.display = "none";
-        } else {
-          showPhone(data.data);
-          document.getElementById("error-msg").style.display = "none";
-          document.getElementById("spinner").style.display = "none";
-        }
-      });
-    document.getElementById("search-box").value = "";
-    document.getElementById("phone-container").innerHTML = "";
-    document.getElementById("phone-details").innerHTML = "";
-  } else {
-    document.getElementById("phone-container").innerHTML = "";
-    document.getElementById("phone-details").innerHTML = "";
-    document.getElementById("error-msg").style.display = "block";
-    document.getElementById("spinner").style.display = "none";
-  }
+  document.getElementById("phone-container").innerHTML = "";
+  document.getElementById("phone-details").innerHTML = "";
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      showPhone(data.status, data.data);
+    });
 };
 
 // -----------------Show phone on UI----------------
 //--------------------------------------------------
-const showPhone = (phones) => {
+const showPhone = (dataAvailable, phonesData) => {
   const phoneContainer = document.getElementById("phone-container");
-  if (phones.length >= 20) {
-    const first20 = phones.filter((_, i) => i < 20);
-    phones = first20;
-  }
+  let phones = phonesData.slice(0, 20);
   console.log(phones);
-  for (const phone of phones) {
-    console.log(phone);
-    const div = document.createElement("div");
-    div.classList.add("col-lg-4");
-    div.classList.add("col-md-6");
-    div.innerHTML = `
-          <div class="card mb-5 mt-5 text-center" style="width: 18rem">
-            <img src="${phone.image}" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h4 class="card-title">${phone.phone_name}</h4>
-              <h5 class="card-title">${phone.brand}</h5>
-              <a href="#" onclick="detailsBtn('${phone.slug}')" class="btn btn-secondary mt-3">Details</a>
+  if (dataAvailable == true) {
+    document.getElementById("error-msg").style.display = "none";
+    document.getElementById("spinner").style.display = "none";
+    for (const phone of phones) {
+      console.log(phone);
+      const div = document.createElement("div");
+      div.classList.add("col-lg-4");
+      div.classList.add("col-md-6");
+      div.innerHTML = `
+            <div class="card border-0 mb-5 mt-5 text-center" style="width: 18rem">
+              <img src="${phone.image}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h4 class="card-title">${phone.phone_name}</h4>
+                <h5 class="card-title">${phone.brand}</h5>
+                <a href="#" onclick="detailsBtn('${phone.slug}')" class="btn btn-outline-secondary px-4 mt-3">Details</a>
+              </div>
             </div>
-          </div>
-          `;
-    phoneContainer.appendChild(div);
+            `;
+      phoneContainer.appendChild(div);
+    }
+  } else {
+    document.getElementById("error-msg").style.display = "block";
+    document.getElementById("spinner").style.display = "none";
   }
 };
 
@@ -72,25 +63,23 @@ const phoneDetails = (phone) => {
   const phnDetails = document.getElementById("phone-details");
   phnDetails.innerHTML = `
   <div class="card mb-5 mt-5 h-100" style="width: 30rem;">
-  <img src="${phone.image}" class="card-img-top" alt="..." />
+  <img src="${
+    phone.image
+  }" class="mx-auto card-img-top w-50 " id="detail-img" alt="..." />
     <div class="card-body">
         <h3 class="card-title text-center">${phone.name}</h3>
         <h4 class="card-title text-center">${phone.brand}</h4>
         <ul id="ul" class="list-group list-group-flush ">
-          <li class="list-group-item text-center">${(phone.releaseDate =
-            phone.releaseDate
-              ? phone.releaseDate
-              : "No release date found")}</li>
-          <li class="list-group-item fw-bold text-center">
-          Main Features
-          </li>
+          <li class="list-group-item text-center">${
+            phone.releaseDate ? phone.releaseDate : "No release date found"
+          }</li>
         </ul>
     </div>
   </div>
 `;
   //----------------- Main features and others data ---------------------
   //-----------------------------------------------------------------------
-  moreInfo(phone.mainFeatures);
+  moreInfo(phone.mainFeatures, phone, false);
   moreInfo(phone.others, phone, true);
 };
 
@@ -98,20 +87,24 @@ const phoneDetails = (phone) => {
 //-----------------------------------------------------------------------
 const moreInfo = (info, phone, enter) => {
   const ul = document.getElementById("ul");
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
   if (enter && phone.others) {
-    const li = document.createElement("li");
-    li.classList.add("list-group-item");
-    li.innerHTML = `<li id='remove-dot' class="fw-bold text-center">Others
+    li.innerHTML = `<li class="fw-bold text-center remove-dot">Others
+    </li>`;
+    ul.appendChild(li);
+  } else if (!enter && phone.mainFeatures) {
+    li.innerHTML = `<li class=" fw-bold text-center remove-dot">
+    Main Features
     </li>`;
     ul.appendChild(li);
   }
   for (const key in info) {
     const li = document.createElement("li");
     li.classList.add("list-group-item");
-    li.innerHTML = `
-                  <span class="bold">  ${
-                    info ? key[0].toUpperCase() + key.slice(1) : ""
-                  }: </span>${(info[key] =
+    li.innerHTML = `<span class="bold">${
+      info ? key[0].toUpperCase() + key.slice(1) : ""
+    }: </span>${(info[key] =
       key === "sensors" ? info[key].join(", ") : info[key])}
               `;
     console.log(info[key]);
